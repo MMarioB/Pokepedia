93cbc4e6bd5d82eaeb28aece161d1c53be4cf97e
Mario <mmariob196@gmail.com>
2022-03-24 10:31:38 +0100

    Initial Commit

---
+++ Pokepedia/PokepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokepediaApp.swift
+//  Pokepedia
+//
+//  Created by Mario Bravo on 24/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PokepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
